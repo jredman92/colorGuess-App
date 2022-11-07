b7314a7e5ed335baeae57fe60f0654ac7ba1060b
jaymie <jredman92@gmail.com>
2022-11-07 05:52:02 -0800

    first commit

---
+++ guessColors/src/App.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+  const getRandomColor = () => {
+    const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',] 
+
+    const color = new Array(6)
+      .fill("")
+      .map(() => digits[Math.floor(Math.random() * digits.length)])
+      .join("");
+
+      return `#${color}`;
+  };
+
+    enum Result {
+      Correct,
+      Wrong,
+  };
+
+  function App() {
+    const [color, setColor] = useState(""); 
+    const [answers, setAnswers] = useState<string[]>([]);
+    const [result, setResult] = useState<Result | undefined>(undefined);
+    
+    const generateColors = () => {
+      const actualColor = getRandomColor();
+      setColor(actualColor);
+      setAnswers(
+        [actualColor, getRandomColor(), getRandomColor()].sort(
+          () => 0.5 - Math.random()
+        )
+      );
+    };
+
+    useEffect(() => {
+      generateColors();
+    }, []);
+
+    const handleAnswerClicked = (answer: string) => {
+      if (answer === color) {
+      setResult(Result.Correct);
+      // TODO: reselect colors
+      generateColors();
+    } else {
+      // TODO: guessed wrong answer
+      setResult(Result.Wrong);
+    };
+  };
+
+    return  (
+    <div className="App">
+      <div>
+
+        <div className="guess-me" style ={{ background:color }}></div>
+      
+        {answers.map(answer => (
+          <button onClick={() => handleAnswerClicked(answer)} key={answer}>
+              {answer}
+            </button>
+        ))}
+        {result === Result.Correct && <div className='correct'>Correct!</div> }
+        {result === Result.Wrong && <div className='wrong'>Wrong Answer</div> }
+      </div>
+    </div>
+    );
+};
+
+export default App
